7663da14108eebd8ecd1578cab29edf9d3db7916
gurrms95 <gurrms95@gmail.com>
2020-08-02 20:54:10 +0900

    start_firebase

---
+++ FireBase/app/src/main/java/com/example/firebase/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference = firebaseDatabase.getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void fire(View view) {
+        databaseReference.child("message").push().setValue("2");
+    }
+}
